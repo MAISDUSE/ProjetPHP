9b5335efce561762878e36a3bc00a174b1ea9333
xiensy <richomme.lucas@gmail.com>
2019-10-17 10:12:25 +0200

    View main + doss css

---
+++ view/main.view.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Vinylooper</title>
+  </head>
+  <body>
+    <header>
+      <nav>
+        <ul>
+          <li><a href="#"></a></li>
+          <li><a href="#"></a></li>
+          <li><a href="#"></a></li>
+          <li><a href="#"></a></li>
+          <li><a href="#"></a></li>
+        </ul>
+      </nav>
+    </header>
+  </body>
+</html>
