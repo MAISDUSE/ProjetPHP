dbdc6a8b723e7e99c96081eeb977146a030e7d4a
xiensy <richomme.lucas@gmail.com>
2019-10-17 10:37:05 +0200

    Main view

--- view/main.view.php
+++ view/main.view.php
@@ -3,18 +3,35 @@
   <head>
     <meta charset="utf-8">
     <title>Vinylooper</title>
+    <link rel="stylesheet" href="/design/style.css">
   </head>
   <body>
+
     <header>
       <nav>
-        <ul>
-          <li><a href="#"></a></li>
-          <li><a href="#"></a></li>
-          <li><a href="#"></a></li>
-          <li><a href="#"></a></li>
-          <li><a href="#"></a></li>
+        <ul class="nav_left">
+          <li><img src="" alt=""></li> <!--Logo-->
+        </ul>
+        <ul class="nav_mid">
+          <li><a href="#">Vinyles</a></li> <!--Vinyles-->
+          <li><a href="#"></a>Matériel</li> <!--Matériel-->
+          <li><a href="#"></a>Assistance</li> <!--FAQ-->
+        </ul>
+        <ul class="nav_right">
+          <li><a href="#"><img src="" alt=""></a></li> <!--Panier-->
+          <li><a href="#"><img src="" alt=""></a></li> <!--Se connecter-->
         </ul>
       </nav>
     </header>
+
+    <section>
+      <div class=""> <!--Nos vinyles-->
+
+      </div>
+      <div class=""> <!--Notre matériel-->
+
+      </div>
+    </section>
+
   </body>
 </html>
