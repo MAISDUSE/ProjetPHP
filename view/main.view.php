ce6904d90c33d03125708922e62b158de8d72427
dusema <dusema@hostname.iut2.upmf-grenoble.fr>
2019-10-22 11:34:29 +0200

    class

--- view/main.view.php
+++ view/main.view.php
@@ -1,41 +1,65 @@
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Vinylooper</title>
-    <link rel="stylesheet" href="/design/style.css">
-  </head>
-  <body>
+<head>
+  <meta charset="utf-8">
+  <title>Vinylooper</title>
+  <link rel="stylesheet" href="/design/style.css">
+</head>
+<body>
 
-    <header>
-      <nav>
-        <ul class="nav_left">
-          <li><img src="" alt="Vinylooper Logo"></li> <!--Logo-->
-        </ul>
-        <ul class="nav_mid">
-          <li><a href="#">Vinyles</a></li> <!--Vinyles-->
-          <li><a href="#">Matériel</a></li> <!--Matériel-->
-          <li><a href="#">Assistance</a></li> <!--FAQ-->
-          <li><a href="#"><img src="" alt="Panier"></a></li> <!--Panier-->
-          <li><a href="#"><img src="" alt="Se connecter"></a></li> <!--Se connecter-->
-        </ul>
-      </nav>
-    </header>
+  <header>
+    <nav>
+      <ul class="nav_left">
+        <li><img src="" alt="Vinylooper Logo"></li> <!--Logo-->
+      </ul>
+      <ul class="nav_mid">
+        <li><a href="#">Vinyles</a></li> <!--Vinyles-->
+        <li><a href="#">Matériel</a></li> <!--Matériel-->
+        <li><a href="#">Assistance</a></li> <!--FAQ-->
+        <li><a href="#"><img src="" alt="Panier"></a></li> <!--Panier-->
+        <li><a href="#"><img src="" alt="Se connecter"></a></li> <!--Se connecter-->
+      </ul>
+    </nav>
+  </header>
 
-    <section>
-      <h2>Vinyles</h2>
-      <div class=""> <!--Nos vinyles-->
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      </div>
+  <section>
+    <h2>Vinyles</h2>
+    <div class=""> <!--Nos vinyles-->
+      <ul class="LesDifferentsVinyles">
+        <?php
+        foreach($genres as $vinyle){ //parcours array list de genreq recupérer depuis bd et on recupére chaque premier vinyle de chaque genre pour avoir illsutration
+          ?>
+          <a href="=<?= $vinyle->getGenre() ?>">
+            <img class="cover" src="<?= $vinyle->getImg() ?>" />
+          </a>
+        <?php } ?>
+      </ul>
+      <!-- <button type="button" name="Voir plus"></button>
+      pour limiter affichage a un nombre defini dans le foreach...-->
+
       <h2>Matériel</h2>
       <div class=""> <!--Notre matériel-->
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <ul class="LesDifferentsMateriels">
+          <?php
+          foreach($types as $matos){ //parcours array list de types recupérer depuis bd et on recupére chaque premier matos de chaque pour illustartion
+            ?>
+            <a href="=<?= $matos->getType() ?>">
+              <img class="cover" src="<?= $matos->getImg() ?>" />
+            </a>
+          <?php } ?>
+        </ul>
+        <!-- <button type="button" name="Voir plus"></button>
+        pour limiter affichage a un nombre defini dans le foreach...-->
       </div>
     </section>
 
     <footer>
-
+      <ul class="footer">
+        <li><a href="#">Assistance</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </nav>
 
     </footer>
   </body>
-</html>
+  </html>
