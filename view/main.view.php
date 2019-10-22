d6a442a8b473e83630ebd4f4f75383a877c2cf4c
DOMINGO Yoann <domingoy@hostname.iut2.upmf-grenoble.fr>
2019-10-22 11:01:29 +0200

    2 nav

--- view/main.view.php
+++ view/main.view.php
@@ -16,23 +16,23 @@
           <li><a href="#">Vinyles</a></li> <!--Vinyles-->
           <li><a href="#">Matériel</a></li> <!--Matériel-->
           <li><a href="#">Assistance</a></li> <!--FAQ-->
-        </ul>
-        <ul class="nav_right">
-          <li><a href="#"><img src="" alt=""></a></li> <!--Panier-->
-          <li><a href="#"><img src="" alt=""></a></li> <!--Se connecter-->
+          <li><a href="#"><img src="" alt="Panier"></a></li> <!--Panier-->
+          <li><a href="#"><img src="" alt="Se connecter"></a></li> <!--Se connecter-->
         </ul>
       </nav>
     </header>
 
     <section>
+      <h2>Vinyles</h2>
       <div class=""> <!--Nos vinyles-->
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
       </div>
+      <h2>Matériel</h2>
       <div class=""> <!--Notre matériel-->
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
       </div>
     </section>
-    
+
     <footer>
 
 
