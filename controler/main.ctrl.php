2d9aba309bb290fa88a81f7baca363f2148139a6
dusema <dusema@hostname.iut2.upmf-grenoble.fr>
2019-10-24 10:29:16 +0200

    main

--- controler/main.ctrl.php
+++ controler/main.ctrl.php
@@ -6,22 +6,16 @@ require_once('../model/MaterielDAO.class.php');
 
 
 // Inclus le mini framework
-//include('../framework/view.class.php');
+include('../framework/view.class.php');
 
 $view = new View("main.view.php");
 
 
-
-
-
-;
 $config = parse_ini_file('../config/config.ini');
 $jukebox = new VinyleDAO($config['database_path']);
 
 
 
-
-//$view->genres= $config['genresVinyles']; //requete sql pour recup genres et on recupére chaque premier vinyle de chaque genre pour avoir illsutration
 
 //$view->types=
 
