9a1b89aa3db075c012ae94068b5bfb25b306ea52
BuildTools <unconfigured@null.spigotmc.org>
2019-11-02 15:34:33 +0100

    css

--- view/main.view.php
+++ view/main.view.php
@@ -8,16 +8,15 @@
 <body>
 
   <header>
+    <img src="" alt="Vinylooper Logo" class="logo"><!--Logo-->
     <nav>
-      <ul class="nav_left">
-        <li><img src="" alt="Vinylooper Logo"></li> <!--Logo-->
-      </ul>
       <ul class="nav_mid">
+
         <li><a href="#">Vinyles</a></li> <!--Vinyles-->
         <li><a href="#">Matériel</a></li> <!--Matériel-->
         <li><a href="#">Assistance</a></li> <!--FAQ-->
-        <li><a href="#"><img src="" alt="Panier"></a></li> <!--Panier-->
-        <li><a href="#"><img src="" alt="Se connecter"></a></li> <!--Se connecter-->
+        <li><a href="#"><img src="../view/design/panier_image.png" alt="Panier" class="boutonsNav"></a></li> <!--Panier-->
+        <li><a href="#"><img src="../view/design/connexion_image.png" alt="Se connecter" class="boutonsNav"></a></li> <!--Se connecter-->
       </ul>
     </nav>
   </header>
