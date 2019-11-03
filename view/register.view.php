e2c3a0cfad0366ed8476ed77f4e01006bc17bbf9
dusema <dusema@hostname.iut2.upmf-grenoble.fr>
2019-11-03 16:42:34 +0100

    merge

---
+++ view/register.view.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang=fr dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title>Vinylooper.Register</title>
+  <link rel="stylesheet" href="../view/design/register.style.css">
+</head>
+<header>
+  <img src="" alt="Vinylooper Logo" class="logo"> <!--Logo-->
+  <nav>
+    <ul class="nav_mid">
+      <li><a href="#">Vinyles</a></li> <!--Vinyles-->
+      <li><a href="#">Matériel</a></li> <!--Matériel-->
+      <li><a href="#">Assistance</a></li> <!--FAQ-->
+      <li><a href="#"><img src="../view/design/panier_image.png" alt="Panier" class="boutonsNav"></a></li> <!--Panier-->
+      <li><a href="#"><img src="../view/design/connexion_image.png" alt="Se connecter" class="boutonsNav"></a></li> <!--Se connecter-->
+    </ul>
+  </nav>
+</header>
+<body>
+
+
+</body>
+</html>
