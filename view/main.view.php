54963b57ffaa3191f0257bae015b5286b124f4b4
dusema <dusema@hostname.iut2.upmf-grenoble.fr>
2019-11-03 16:27:13 +0100

    register v1

--- view/main.view.php
+++ view/main.view.php
@@ -15,7 +15,7 @@
         <li><a href="#">Matériel</a></li> <!--Matériel-->
         <li><a href="#">Assistance</a></li> <!--FAQ-->
         <li><a href="#"><img src="../view/design/panier_image.png" alt="Panier" class="boutonsNav"></a></li> <!--Panier-->
-        <li><a href="#"><img src="../view/design/connexion_image.png" alt="Se connecter" class="boutonsNav"></a></li> <!--Se connecter-->
+        <li><a href="register.ctrl.php"><img src="../view/design/connexion_image.png" alt="Se connecter" class="boutonsNav"></a></li> <!--Se connecter-->
       </ul>
     </nav>
   </header>
@@ -28,8 +28,8 @@
         foreach($genrespres as $vinyle){ //Parcours array list des genres de vinyle récuperés depuis la bd et on recupère chaque premier vinyle de chaque genre pour avoir l'illsutration
           ?>
           <li>
-            <a href=""><img class="cover" src="../model/data/img/<?= $vinyle->getImg() ?>" alt="<?= $vinyle->getIntitule()?>"/></a>
             <a href=""><p><?= $vinyle->getGenre() ?></p></a>
+            <a href=""><img class="cover" src="../model/data/img/<?= $vinyle->getImg() ?>" alt="<?= $vinyle->getIntitule()?>"/></a>
           </li>
         <?php } ?>
       </ul>
@@ -41,8 +41,8 @@
           foreach($typespres as $matos){ //Parcours array list des genres de vinyle récuperés depuis la bd et on recupère chaque premier vinyle de chaque genre pour avoir l'illsutration
             ?>
             <li>
-              <a href=""><img class="cover" src="../model/data/img/<?= $matos->getImg() ?>" alt="<?= $matos->getIntitule()?>"/></a>
               <a href=""><p><?= $matos->getType() ?></p></a>
+              <a href=""><img class="cover" src="../model/data/img/<?= $matos->getImg() ?>" alt="<?= $matos->getIntitule()?>"/></a>
             </li>
           <?php } ?>
         </ul>
