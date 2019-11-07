9543fbfc793534f2e9342872ec3f363e6b4ebf90
dusema <dusema@hostname.iut2.upmf-grenoble.fr>
2019-11-07 15:35:38 +0100

    appel lien page panier

--- view/vinyle.view.php
+++ view/vinyle.view.php
@@ -15,7 +15,7 @@
         <ul id="nav_mid">
           <li><a href="../controler/catVinyle.ctrl.php">Vinyles</a></li> <!--Vinyles-->
           <li><a href="../controler/catMateriel.ctrl.php">Matériel</a></li> <!--MatÃ©riel-->
-          <li><a href="#" id="panier">Panier</a></li> <!--Panier--> <!--<img src="../view/design/panier_image.png" alt="Panier" class="boutonsNav">-->
+          <li><a href="../controler/panier.ctrl.php" id="panier">Panier</a></li> <!--Panier--> <!--<img src="../view/design/panier_image.png" alt="Panier" class="boutonsNav">-->
           <li><a href="../controler/register.ctrl.php">Se connecter</a></li> <!--Se connecter--> <!--<img src="../view/design/connexion_image.png" alt="Se connecter" class="boutonsNav">-->
         </ul>
       </nav>
