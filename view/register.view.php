a8fe1155c505bd488415ed6e555becaf39145f8a
dusema <dusema@hostname.iut2.upmf-grenoble.fr>
2019-11-07 12:37:30 +0100

    section + user mathieu@mail.fr mama

--- view/register.view.php
+++ view/register.view.php
@@ -21,40 +21,50 @@
       </ul>
     </nav>
   </header>
-  <?php
-  if(isset($_SESSION['email']) && isset($_SESSION['nom']) && isset($_SESSION['prenom'])){
-    ?>
-    <h1>Bienvenue sur votre profil</h1>
-    <p>Votre Email :<?=$_SESSION['email'];?></p>
-    <p>Votre Nom :<?=$_SESSION['nom'];?></p>
-    <p>Votre Prenom :<?=$_SESSION['prenom'];?></p>
-    <form method="post">
-      <input type="submit" name="deconnexion" id="deconnexion" value="Se deconnecter">
-    </form>
+  <section>
+
 
     <?php
-  }else {
+    if(isset($_SESSION['email']) && isset($_SESSION['nom']) && isset($_SESSION['prenom'])){
+      ?>
+      <h1>Bienvenue sur votre profil</h1>
+      <p>Votre Email :<?=$_SESSION['email'];?></p>
+      <p>Votre Nom :<?=$_SESSION['nom'];?></p>
+      <p>Votre Prenom :<?=$_SESSION['prenom'];?></p>
+      <form method="post">
+        <input type="submit" name="deconnexion" id="deconnexion" value="Se deconnecter">
+      </form>
+
+      <?php
+    }else {
+      ?>
+      <p>Veuillez vous connecter à votre compte</p>
+    <?php   }
     ?>
-    <p>Veuillez vous connecter à votre compte</p>
-  <?php   }
-  ?>
-  <h2>Connexion - Login</h2>
-  <form method="post">
-    <input type="email" name="lemail" id="lemail" placeholder="Votre email" required><br>
-    <input type="password" name="lpassword" id="lpassword" placeholder="Votre Mot de passe" required><br>
-    <input type="submit" name="formlogin" id="formlogin" value="Se connecter">
-  </form>
-  <p><?= $etat ?></p>
+    <h2>Connexion - Login</h2>
+    <form method="post">
+      <input type="email" name="lemail" id="lemail" placeholder="Votre email" required><br>
+      <input type="password" name="lpassword" id="lpassword" placeholder="Votre Mot de passe" required><br>
+      <input type="submit" name="formlogin" id="formlogin" value="Se connecter">
+    </form>
+    <p><?= $etat ?></p>
 
 
-  <h2>Inscription - Signin</h2>
-  <form method="post">
-    <input type="text" name="nom" placeholder="Votre Nom" required><br>
-    <input type="text" name="prenom" id="prenom" placeholder="Votre prenom" required><br>
-    <input type="email" name="semail" id="semail" placeholder="Votre email" required><br>
-    <input type="password" name="password" id="password" placeholder="Votre Mot de passe" required><br>
-    <input type="password" name="cpassword" id="cpassword" placeholder="Confirmer votre Mot de passe" required><br>
-    <input type="submit" name="formsend" id="formsend" value="S'inscrire">
-  </form>
-  <p><?= $etat2 ?></p>
+    <h2>Inscription - Signin</h2>
+    <form method="post">
+      <input type="text" name="nom" placeholder="Votre Nom" required><br>
+      <input type="text" name="prenom" id="prenom" placeholder="Votre prenom" required><br>
+      <input type="email" name="semail" id="semail" placeholder="Votre email" required><br>
+      <input type="password" name="password" id="password" placeholder="Votre Mot de passe" required><br>
+      <input type="password" name="cpassword" id="cpassword" placeholder="Confirmer votre Mot de passe" required><br>
+      <input type="submit" name="formsend" id="formsend" value="S'inscrire">
+    </form>
+    <p><?= $etat2 ?></p>
+  </section>
+  <footer>
+    <ul class="footer">
+      <li>Copyright (c) 2019 Vinylooper</li>
+      <li><a href="#">Nous contacter</a></li>
+    </ul>
+  </footer>
 </body>
